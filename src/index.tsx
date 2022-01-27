2752352e01c04f91b773df47bf8995d4ba15f01c
jinhyukoo <faill456@naver.com>
2022-01-27 18:29:16 +0900

    🤡 : set mock file

---
+++ src/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import '../public/index.scss';
+import Greetings from './component';
+
+ReactDOM.render(
+  <Greetings name="jinhyukoo"></Greetings>,
+  document.getElementById('root')
+)
