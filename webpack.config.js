68930743c968daca5988b67cea7ac17667796fcd
jinhyukoo <faill456@naver.com>
2022-01-27 18:28:07 +0900

    🔧 : update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
-  entry: './src/index.ts',
+  entry: './src/index.tsx',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
