e56031cee68f07b7d566e11f35e4ae1c071f5815
jinhyukoo <faill456@naver.com>
2022-01-28 11:00:37 +0900

    🔨 : apply store

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import '../public/index.scss';
 import Greetings from './component';
+import { store } from './app/store'
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
-  <Greetings name="jinhyukoo"></Greetings>,
+  <Provider store={store}>
+    <Greetings name="jinhyukoo"></Greetings>
+  </Provider>,
   document.getElementById('root')
 )
